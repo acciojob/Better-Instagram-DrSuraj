a922e7425678c541675123ad6a518ffc9f244c40
SURAJ <77872306+DrSuraj@users.noreply.github.com>
2024-01-13 17:48:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+
+function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    var draggedElement = document.getElementById(data);
+
+    // Swap background images
+    var tempImage = draggedElement.style.backgroundImage;
+    draggedElement.style.backgroundImage = event.target.style.backgroundImage;
+    event.target.style.backgroundImage = tempImage;
+}
